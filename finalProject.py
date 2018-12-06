a0c335f3a8834c87720ee9fae4f0c9834a91870d
vagrant <vagrant@vagrant.vm>
2018-12-06 22:56:27 +0000

    Add page templates

--- finalProject.py
+++ finalProject.py
@@ -1,21 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Posts, Categories, Users, Comments
+import datetime
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///forum_database.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
 
 @app.route('/')
 @app.route('/categories')
 def showCategories():
-    return 'Show all categories here'
+    session = DBSession()
+    categories = session.query(Categories).all()
+    session.close()
+    return render_template('categories.html', categories=categories)
 
-@app.route('/categories/new')
+
+@app.route('/categories/new', methods=['GET', 'POST'])
 def createCategory():
-  return 'Create a new category here'
+    if request.method == 'POST':
+        session = DBSession()
+        newCategory = Categories(name=request.form['title'])
+        if (session.query(Categories).
+            filter_by(name=request.form['title']).first()) == None:
+            session.add(newCategory)
+            session.commit()
+            flash('Category added!')
+            session.close()
+            return redirect(url_for('showCategories'))
+        else:
+            flash('Category with that name already exists')
+            return redirect(url_for('createCategory'))
+    else:
+        return render_template('newCategory.html')
+
 
 @app.route('/categories/<int:category_id>')
 def showCategoryPosts(category_id):
-  output = ""
-  output += "Showing category #"
-  output += str(category_id)
-  return output
+    session = DBSession()
+    category = session.query(Categories).filter_by(id=category_id).one()
+    posts = session.query(Posts).filter_by(category_id=category_id)
+    session.close()
+    return render_template('posts.html', category = category, posts = posts)
+
 
 @app.route('/categories/<int:category_id>/edit')
 def editCategory(category_id):
@@ -24,6 +56,7 @@ def editCategory(category_id):
   output += str(category_id)
   return output
 
+
 @app.route('/categories/<int:category_id>/delete')
 def deleteCategory(category_id):
   output = ""
@@ -31,13 +64,37 @@ def deleteCategory(category_id):
   output += str(category_id)
   return output
 
-@app.route('/categories/<int:category_id>/add')
+
+@app.route('/categories/<int:category_id>/<int:post_id>', methods=['GET', 'POST'])
+def showPost(category_id, post_id):
+    if request.method == 'POST':
+        session = DBSession()
+        newComment = Comments(content=request.form['content'], post_id=post_id, time=datetime.datetime.now())
+        session.add(newComment)
+        session.commit()
+        session.close()
+        return redirect(url_for('showPost', category_id=category_id, post_id=post_id))
+    session = DBSession()
+    category = session.query(Categories).filter_by(id=category_id).one()
+    post = session.query(Posts).filter_by(id=post_id).one()
+    comments = session.query(Comments).filter_by(post_id=post_id).all()
+    users = session.query(Users).all()  
+    session.close()
+    return render_template('post.html', category = category, post = post, users=users ,comments = comments)
+
+
+@app.route('/categories/<int:category_id>/add', methods=['GET', 'POST'])
 def addPost(category_id):
-  output = ""
-  output += "Adding Post "
-  output += "in category #"
-  output += str(category_id)
-  return output
+    if request.method == 'POST':
+            session = DBSession()
+            newPost = Posts(title=request.form['title'], content=request.form[
+                               'content'], category_id=category_id, time=datetime.datetime.now())
+            session.add(newPost)
+            session.commit()
+            session.close()
+            return redirect(url_for('showCategoryPosts', category_id=category_id))
+    else:
+            return render_template('newPost.html', category_id=category_id)
 
 @app.route('/categories/<int:category_id>/edit/<int:post_id>')
 def editPost(category_id, post_id):
@@ -48,6 +105,7 @@ def editPost(category_id, post_id):
   output += str(category_id)
   return output  
 
+
 @app.route('/categories/<int:category_id>/delete/<int:post_id>')
 def deletePost(category_id, post_id):
   output = ""
@@ -57,9 +115,11 @@ def deletePost(category_id, post_id):
   output += str(category_id)
   return output
 
+
 @app.route('/login')
 def login():
   return 'Log in here'
+
 
 @app.route('/logout')
 def logout():
@@ -69,10 +129,25 @@ def logout():
 def registerUser():
   return 'Register here'
 
+
 @app.route('/admin')
 def showAdminPanel():
-  return 'Admin panel here'
+    return 'Admin panel here'
+
+
+@app.route('/profiles/<int:user_id>/')
+def showUserProfile(user_id):
+    return 'Showing user details here'
+
+
+def searchUserPhotoURL(user_id):
+    session = DBSession()
+    url = session.query(Users).filter_by(id=user_id).one().photoURL
+    session.close()
+    return url
+
 
 if __name__ == '__main__':
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8000)
